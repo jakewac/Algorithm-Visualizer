5c8b15b68d46ea8379a5f422af0143f4ac2537af
Jacob Waclawski <54681308+jmw3638@users.noreply.github.com>
2021-01-25 14:24:49 -0500

    Edits to css files

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import { Card, CardHeader, CardBody, CardFooter
+} from 'reactstrap';
+
+import './Home.css';
+
 /**
  * Represents the home tab component.
  * 
@@ -19,11 +24,24 @@ class Home extends React.Component {
     render () {
         return (
             <div>
-                <h1>Algorithm Visualizer</h1>
-                <h3>Jake Waclawski</h3>
-                <a href="https://github.com/jmw3638/Pathfinder">GitHub</a><br />
-                <button onClick={() => this.props.changeTab("Pathfinding Visualizer")}>Pathfinding Visualizer</button>
-                <button onClick={() => this.props.changeTab("Sorting Visualizer")}>Sorting Visualizer</button>
+                <Card>
+                    <CardHeader>
+                        <h1>Algorithm Visualizer</h1>
+                        <h4>Jake Waclawski</h4>
+                        <a href="https://github.com/jmw3638/Pathfinder">GitHub</a><br />
+                    </CardHeader>
+                    <CardBody>
+                        
+                    </CardBody>
+                    <CardFooter>
+                        <div className="home-visualizer-button"
+                        onClick={() => this.props.changeTab("Pathfinding Visualizer")}
+                        >Pathfinding Visualizer</div>
+                        <div className="home-visualizer-button"
+                        onClick={() => this.props.changeTab("Sorting Visualizer")}
+                        >Sorting Visualizer</div>
+                    </CardFooter>
+                </Card>
             </div>
         );
     }
